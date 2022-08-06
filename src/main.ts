e4555bffc37b5e9af0b2d20b50259f297a5046f9
Calvin Tsang <calvin.hs.tsang@prudential.com.hk>
2022-08-06 19:37:39 +0800

    apply test case
    create fake swagger application, also apply test case for test running in pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
+import { AppModule } from "./app.module";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 async function bootstrap() {
     console.log('init');
+    const serviceName = 'localhost:8080'
+    const serviceVersion = '1'
+    const app = await NestFactory.create(AppModule, {cors: true})
+    const options = new DocumentBuilder().addBearerAuth().setTitle(serviceName).setVersion(serviceVersion).build();
+    SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, options));
     console.log('running application')
 }
 
